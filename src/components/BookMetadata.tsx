e618b9cd073a4e5d82d6d734b7b78256c7023e77
Adam Akhlaq <adam.akhlaq@bp.com>
2025-07-19 16:25:58 +0100

    style: change "not started" status apperance

--- src/components/BookMetadata.tsx
+++ src/components/BookMetadata.tsx
@@ -41,7 +41,7 @@ export const BookMetadata: React.FC<BookMetadataProps> = ({ book }) => {
 									? "bg-green-100 text-green-800"
 									: book.status === "In Progress"
 									? "bg-yellow-100 text-yellow-800"
-									: "bg-gray-100 text-gray-800"
+									: "bg-red-100 text-red-800"
 							}`}
 						>
 							{book.status}
