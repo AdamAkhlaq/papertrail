7c801ba6f16c17d96f05b3baf0438c91df3c9b3d
Adam Akhlaq <adam.akhlaq@bp.com>
2025-07-19 19:45:11 +0100

    feat: add basic navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import "./App.css";
 import Home from "./pages/Home";
 import BookPage from "./pages/BookPage";
 import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
 
 function App() {
 	return (
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/books/:slug" element={<BookPage />} />
-			<Route path="*" element={<div>404 Not Found</div>} />
-		</Routes>
+		<>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/books/:slug" element={<BookPage />} />
+				<Route path="*" element={<div>404 Not Found</div>} />
+			</Routes>
+		</>
 	);
 }
 
